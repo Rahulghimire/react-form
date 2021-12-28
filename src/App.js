3b224c973d34ef34ca9086c249655d756ef94836
raul <ghimirerahul554@gmail.com>
2021-12-29 03:35:44 +0545

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
+import "./App.css";
+import DataTable from "react-data-table-component";
 
 function App() {
+  const [persons, setPersons] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState(0);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    const getPersons = async () => {
+      const response = await axios.get("https://api.najikai.com/api/v2/rahul");
+      if (response.data.status) {
+        const p = response.data.data.persons;
+        setPersons(p);
+      }
+    };
+    getPersons();
+  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    const postData = {
+      name,
+      email,
+      phoneNumber,
+      address: {
+        country,
+        state,
+        city,
+      },
+    };
+    console.log(postData);
+    if (state === 0) {
+      alert("State should not be 0");
+      return;
+    }
+    await axios
+      .post(`https://api.najikai.com/api/v2/rahul`, postData)
+      .then((response) => {
+        if (response.data.status) {
+          alert("Success");
+        } else {
+          alert("fail");
+        }
+      });
+  };
+  //Phone Number,Country,state,city
+  const columns = [
+    {
+      name: "Full Name",
+      selector: "name",
+      sortable: true,
+    },
+    {
+      name: "E-mail",
+      selector: "email",
+      sortable: true,
+    },
+    {
+      name: "PhoneNumber",
+      selector: "phoneNumber",
+      sortable: true,
+    },
+    {
+      name: "Country",
+      selector: "address.country",
+      sortable: true,
+    },
+    {
+      name: "State",
+      selector: "address.state",
+      sortable: true,
+    },
+    {
+      name: "city",
+      selector: "address.city",
+      sortable: true,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {persons.map((person, idx) => {
+          return (
+            <div>
+              <li key={idx}>{person.name}</li>
+              <p>{moment(person.createdAt).fromNow()}</p>
+              {/* <p>{moment(person.updatedAt).fromNow()}</p> */}
+            </div>
+          );
+        })}
+      </ul>
+      <form action="" onSubmit={handleSubmit}>
+        <div style={{ padding: "10px" }}>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>Phone number:</label>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
+          />
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>Country:</label>
+          <input
+            type="text"
+            required
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>State:</label>
+          <input
+            required
+            type="number"
+            value={state}
+            onChange={(e) => setState(parseInt(e.target.value))}
+          />
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>City:</label>
+          <input
+            type="text"
+            required
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <input type="submit" />
+      </form>
+      {/* <DataTable columns={columns} data={data} /> */}
+      <DataTable columns={columns} data={persons} />
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/DataFetching.js
@@ -0,0 +1,22 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// const DataFetching = () => {
+//   const [data, setData] = useState({});
+//   useEffect(() => {
+//     axios.get("https://api.najikai.com/api/v2/rahul").then((res) => {
+//       console.log(res.data);
+//       setData(res.data);
+//     });
+//   }, []);
+//   // const persons = data?.data?.persons;
+//   return (
+//     <div>
+//       <ul>
+//         {data.map((values) => {
+//           return <li>{values.name}</li>;
+//         })}
+//       </ul>
+//     </div>
+//   );
+// };
+// export default DataFetching;
